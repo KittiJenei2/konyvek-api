e50d10a2b2daa463c2cfc507a2c54751148fc623
Kitti <152151102+KittiJenei2@users.noreply.github.com>
2025-10-21 13:51:16 +0200

    Videó vége

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\WriterController;
-//use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/users/login', [UsersController::class, 'login']);
@@ -11,3 +10,7 @@ Route::get('/users', [UsersController::class, 'index'])->middleware('auth:sanctu
 Route::get('/writers', [WriterController::class, 'index']);
 
 Route::post('/writers', [WriterController::class, 'store'])->middleware('auth:sanctum');;
+
+Route::patch('/writers/{id}', [WriterController::class, 'update'])->middleware('auth:sanctum');
+
+Route::delete('/writers/{id}',[WriterController::class, 'destroy'])->middleware('auth:sanctum');
