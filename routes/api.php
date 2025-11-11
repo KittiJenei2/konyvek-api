a562da4821b2d4f1c19922a60afa49e23f0dc539
Kitti <152151102+KittiJenei2@users.noreply.github.com>
2025-11-11 14:41:12 +0100

    Módosítások

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,25 @@
 
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\WriterController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
+use NunoMaduro\Collision\Writer;
 
 Route::post('/users/login', [UsersController::class, 'login']);
 Route::get('/users', [UsersController::class, 'index'])->middleware('auth:sanctum');
 
 Route::get('/writers', [WriterController::class, 'index']);
 
-Route::post('/writers', [WriterController::class, 'store'])->middleware('auth:sanctum');;
+Route::post('/writers', [WriterController::class, 'store'])->middleware('auth:sanctum');
 
 Route::patch('/writers/{id}', [WriterController::class, 'update'])->middleware('auth:sanctum');
 
 Route::delete('/writers/{id}',[WriterController::class, 'destroy'])->middleware('auth:sanctum');
+
+Route::get('/writers/{author_id}/books', [WriterController::class, 'index']);
+
+Route::post('/writers/{author_id}/books', [WriterController::class, 'store'])->middleware('auth:sanctum');
+
+Route::patch('/writers/{author_id}/books/{id}', [WriterController::class, 'update'])->middleware('auth:sanctum');
+
+Route::delete('/writers/{author_id}/books/{id}',[WriterController::class, 'destroy'])->middleware('auth:sanctum');
